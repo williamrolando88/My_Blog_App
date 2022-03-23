b18232dd38e089cbdcbda95e9db4c3883732f211
williamrolando88 <william.rolando88@hotmail.com>
2022-03-23 09:17:33 -0500

    Fix: Routes prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'post_id'
+  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show', as: 'user_id'
+  get '/users/:id', to: 'users#show', as: 'user'
   root :to => 'users#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
