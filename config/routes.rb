60b2a54f95795a2776a1a074399cd87fbf9224e7
williamrolando88 <william.rolando88@hotmail.com>
2022-03-23 09:54:03 -0500

    Delete: Comment code block

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  # get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-  # get '/users', to: 'users#index'
-  # get '/users/:id', to: 'users#show', as: 'user'
-  # root :to => 'users#index'
-
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
