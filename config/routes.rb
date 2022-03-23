106099454fb09aca73691333aff84b445c2fe1ec
williamrolando88 <william.rolando88@hotmail.com>
2022-03-23 09:24:51 -0500

    Fix: Linter

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # get '/users/:id', to: 'users#show', as: 'user'
   # root :to => 'users#index'
 
-   resources :users, only: [:index, :show] do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
