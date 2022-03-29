92ca7132b3e9fdd1553343b792266412358e6380
williamrolando88 <william.rolando88@hotmail.com>
2022-03-28 19:40:26 -0500

    Add: Method to update the posts_counter property for its author

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
+
+  def update_post_counter
+    author_posts = Post.where(author_id: author).count
+    author.update(posts_counter: author_posts)
+  end
 end
