66b61d54172230962a6f7654014bbae386988e08
williamrolando88 <william.rolando88@hotmail.com>
2022-03-30 16:46:06 -0500

    Add: Create new post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,5 +14,17 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new(params.require(:post).permit(:title, :text))
+    @post.comments_counter = 0
+    @post.likes_counter = 0
 
+    if @post.save
+      redirect_to user_posts_path(@user)
+    else
+      flash[:notice] = "Post was not created."
+      render :new
+    end
+  end
 end
