d0d30b60419ab5aa63acddcbf261e20ed130060d
williamrolando88 <william.rolando88@hotmail.com>
2022-03-30 21:54:28 -0500

    Add: Create new like action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,16 @@
 class LikesController < ApplicationController
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @like = Like.new(author: @user, post: @post)
+
+    if @like.save
+      redirect_to user_post_path(@user, @post)
+    else
+      flash[:notice] = "Error!"
+    end
+
+    @like.likes_counter
+  end
 end
