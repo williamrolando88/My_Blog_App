270cc1a6c265d4971ca76f47f382f08faeb81cab
williamrolando88 <william.rolando88@hotmail.com>
2022-03-31 08:43:05 -0500

    Fix: Reviewer observations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,19 @@
 class CommentsController < ApplicationController
   def create
+    
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.new(params.require(:comment).permit(:text))
-    @comment.author_id = @user.id
+    @comment.author_id = @current_user.id
     @comment.post_id = @post.id
 
     if @comment.save
       flash[:notice] = "Comment created!"
+      @comment.comments_counter
       redirect_to user_post_path(@user, @post)
     else
       flash[:notice] = "Comment was not created."
     end
 
-    @comment.comments_counter
   end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,14 @@ class LikesController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
-    @like = Like.new(author: @user, post: @post)
+    @like = Like.new(author: @current_user, post: @post)
 
     if @like.save
+      @like.likes_counter
       redirect_to user_post_path(@user, @post)
     else
       flash[:notice] = "Error!"
     end
 
-    @like.likes_counter
   end
 end
