079e3e34da0981a9f07b76f8191ddcddeeabcacb
williamrolando88 <william.rolando88@hotmail.com>
2022-04-04 18:09:09 -0500

    Fix: Logging out issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
-  def after_sign_out_path_for(_resource_or_scope)
-    new_user_session_path
-  end
 end
