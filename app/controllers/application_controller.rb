878ac04b125c949054a8c312b401f629288ef1d1
keroloussamy <keroloussamy98@gmail.com>
2022-04-05 00:29:45 +0200

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
-
 end
