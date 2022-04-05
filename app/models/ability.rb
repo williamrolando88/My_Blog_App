2138c31bc183613a545465537fc6ca3e758088e7
williamrolando88 <william.rolando88@hotmail.com>
2022-04-05 18:00:36 -0500

    Fix: Linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,14 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     can :read, Post
     return unless user.present?
+
     can :manage, Post, author_id: user.id
     can :manage, Comment, author_id: user.id
     return unless user.admin?
+
     can :manage, :all
   end
 end
