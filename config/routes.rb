2bb8b8099e618fe371bbc19a8e3532e826d92490
williamrolando88 <william.rolando88@hotmail.com>
2022-04-05 15:18:42 -0500

    Add: Destroy path for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do
-      resources :comments, only: [:create]
-      resources :likes, only: [:create]
+      resources :comments, only: [:create, :destroy]
+      resources :likes, only: [:create, :destroy]
     end
   end
 
